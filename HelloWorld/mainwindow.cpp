eafc9e35d3cf4e8152a811edc2fc8b4b54bb35a2
JameryLin <835127367@qq.com>
2018-06-11 15:20:08 +0800

    添加了按钮事件

--- HelloWorld/mainwindow.cpp
+++ HelloWorld/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDialog"
+#include "QLabel"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +13,14 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QDialog *dia=new QDialog;
+    dia->setWindowTitle("New Dialog");
+    dia->setFixedSize(200,100);
+    QLabel *lable=new QLabel(dia);
+    lable->setText("Go on!");
+    dia->show();
 }
